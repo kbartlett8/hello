1c48ca9a5d955373208e058b864b53ff236861df
kbartlett8 <kbartlett8@husker.com>
2024-08-29 22:09:31 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Creator: Kayla Bartlett
+ * Date: 8/29/2024
+ *
+ * A program that prints out my name, Kayla Bartlett.
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kayla Bartlett\n");
+
+  return 0;
+}
